b31ef40fb243826f9b9188407fcae687c0c2fd7c
Kelvin Nii Ayi Aryee <aryeekelvin43@gmail.com>
2022-12-11 07:59:07 -0800

    task ten

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,22 +10,19 @@
 int main(void)
 
 {
-
 	int ones = '0';
-
 	int tens = '0';
 
 	for (tens = '0'; tens <= '9'; tens++)/* prints tens digit*/
-
 	{
-
 		for (ones = '0'; ones <= '9'; ones++)/* prints ones digit*/
-		{												if (!((ones == tens) || (tens > ones)))/*eliminates repitition*/
-			    {
-				    putchar(tens);
-				    putchar(ones);
-				    if (!(ones == '9' && tens == '8'))/*addes comma and spacee*/
-				    {
+		{
+			if (!((ones == tens) || (tens > ones)))/*eliminates repitition*/
+			{
+				putchar(tens);
+				putchar(ones);
+				if (!(ones == '9' && tens == '8'))/*addes comma and space*/
+				{
 					    putchar(',');
 					    putchar(' ');
 				    }
