9e37952832735acff085084664dc1888e41ca841
Kelvin Nii Ayi Aryee <aryeekelvin43@gmail.com>
2022-12-14 08:20:49 -0800

    task five

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,12 +2,12 @@
 
 /**
  * print_sign - return 0 letter not lowercase, 1 letter lowercase
- *
  * @n : number to check
  * Return: 0 or 1
  */
 
 int print_sign(int n)
+
 {
 	int test;
 
@@ -22,6 +22,7 @@ int print_sign(int n)
 		_putchar('0');
 	}
 	else
+	{
 		test = -1;
 		_putchar('-');
 	}
