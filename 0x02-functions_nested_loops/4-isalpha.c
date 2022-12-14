603d0fd77e7eeb7904f29c08edf5b512f30be294
Kelvin Nii Ayi Aryee <aryeekelvin43@gmail.com>
2022-12-14 07:57:44 -0800

    task four

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isalpha - Return 1 if c is a letter. lowercase or uppercase
+ * @c: unsigned int value to be compared with ASCII value
+ * Return: 1 if its alphabet otherwise
+ */
+
+int _isalpha(int c)
+
+{
+	if (c >= 65 && c <= 122)
+		return (1);
+	else
+		return (0);
+
+}
+
