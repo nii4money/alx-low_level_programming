fa3361972ca2e3172f25972003e0fde067ab0923
Kelvin Nii Ayi Aryee <aryeekelvin43@gmail.com>
2022-12-14 10:02:26 -0800

    task eleven

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- * print_to_98 - prints all natural numbers to 98
- *
- * @n: number provided by user
+ * print_to_98 - prints all natural numbers
+ * from n to 98, followed by a new line
+ * @n: input number.
+ * Return: no return.
  */
 
 void print_to_98(int n)
